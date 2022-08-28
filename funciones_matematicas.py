ceeb7354370c8cea613c79361508c04bac84f000
Monica Qin <moniq0108@hotmail.com>
2022-08-27 21:23:16 -0300

    agregar multiplicacion

--- funciones_matematicas.py
+++ funciones_matematicas.py
@@ -2,4 +2,7 @@ def sumar(a,b):
     return a+b
 def restar(a,b):
     return a-b
-    
+
+def multiplicar (a,b):
+    return a*b
+
---
+++ uso.py
@@ -0,0 +1,4 @@
+from funciones_matematicas import*
+
+rdo= sumar(1,2)
+print(rdo)
